a1e8ef6120f76e6386ec5220091a16d219cb13f0
kjerand <kjerand.evje@gmail.com>
2022-09-25 10:34:52 +0200

    setup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import React from "react";
+import NavBar from "./components/NavBar.tsx";
+
 const App = () => {
-  return <div>Test</div>;
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+const NavBar = () => {
+  return (
+    <nav className=" bg-[#51abcb] w-full flex relative justify-between items-center mx-auto px-8 h-20"></nav>
+  );
+};
+
+export default NavBar;
